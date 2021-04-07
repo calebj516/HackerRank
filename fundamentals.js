64c3736c2136b5fa3145d87d8e1eb98815b3cc8a
Caleb Jones <calebj516@gmail.com>
2021-04-06 20:10:29 -0500

    Day 1: Functions

--- fundamentals.js
+++ fundamentals.js
@@ -1,29 +1,19 @@
-/**
- *   Calculate the area of a rectangle.
- *
- *   length: The length of the rectangle.
- *   width: The width of the rectangle.
- *
- *	Return a number denoting the rectangle's area.
- **/
+// Task
 
-function getArea(length, width) {
-  let area;
-  area = length * width;
-  return area;
-}
+// Implement a function named factorial that has one parameter: an integer, . It must return the value of  (i.e.,  factorial).
 
-/**
- *   Calculate the perimeter of a rectangle.
- *
- *	length: The length of the rectangle.
- *   width: The width of the rectangle.
- *
- *	Return a number denoting the perimeter of a rectangle.
- **/
+// Input Format
 
-function getPerimeter(length, width) {
-  let perimeter;
-  perimeter = 2 * (length + width);
-  return perimeter;
-}
+// Locked stub code in the editor reads a single integer, , from stdin and passes it to a function named factorial.
+
+const factorial = (n) => {
+  // the factorial of 0 and 1 is 1
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  // loop through each value, starting at one less than n and ending at 1
+  for (let i = n - 1; i >= 1; i--) {
+    n *= i;
+  }
+  return n;
+};
