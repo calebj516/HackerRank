30fe4b4ef287b6d9c150af9cbc3cab3dae74d805
Caleb Jones <calebj516@gmail.com>
2021-04-06 19:50:57 -0500

    Day 1: Arithmetic Operators

--- fundamentals.js
+++ fundamentals.js
@@ -1,38 +1,29 @@
-// Objective
+/**
+ *   Calculate the area of a rectangle.
+ *
+ *   length: The length of the rectangle.
+ *   width: The width of the rectangle.
+ *
+ *	Return a number denoting the rectangle's area.
+ **/
 
-// In this challenge, we practice looping over the characters of string.
+function getArea(length, width) {
+  let area;
+  area = length * width;
+  return area;
+}
 
-// Task
+/**
+ *   Calculate the perimeter of a rectangle.
+ *
+ *	length: The length of the rectangle.
+ *   width: The width of the rectangle.
+ *
+ *	Return a number denoting the perimeter of a rectangle.
+ **/
 
-// First, print each vowel in  on a new line. The English vowels are a, e, i, o, and u, and each vowel must be printed in the same order as it appeared in .
-// Second, print each consonant (i.e., non-vowel) in  on a new line in the same order as it appeared in .
-
-const vowelsAndConsonants = (s) => {
-  let length = s.length;
-
-  for (let count = 0; count < length; count++) {
-    if (
-      s[count] === "a" ||
-      s[count] === "e" ||
-      s[count] === "i" ||
-      s[count] === "o" ||
-      s[count] === "u"
-    ) {
-      console.log(s[count]);
-    }
-  }
-
-  for (let count = 0; count < length; count++) {
-    if (
-      s[count] !== "a" &&
-      s[count] !== "e" &&
-      s[count] !== "i" &&
-      s[count] !== "o" &&
-      s[count] !== "u"
-    ) {
-      console.log(s[count]);
-    }
-  }
-};
-
-vowelsAndConsonants("javascriptloops");
+function getPerimeter(length, width) {
+  let perimeter;
+  perimeter = 2 * (length + width);
+  return perimeter;
+}
