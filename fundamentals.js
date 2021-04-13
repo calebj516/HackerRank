4e86f4d5e41b67029ec02032cb2c8971ec6a5277
Caleb Jones <calebj516@gmail.com>
2021-04-12 21:42:00 -0500

    Day 2: Conditional Statements: Switch

--- fundamentals.js
+++ fundamentals.js
@@ -1,19 +1,51 @@
+// Day 2: Conditional Statements: Switch
+
 // Task
 
-// Implement a function named factorial that has one parameter: an integer, . It must return the value of  (i.e.,  factorial).
+// Complete the getLetter(s) function in the editor. It has one parameter: a string, s, consisting of lowercase English alphabetic letters (i.e., a through z). It must return A, B, C, or D depending on the following criteria:
 
-// Input Format
+// If the first character in string s is in the set {a,e,i,o,u}, then return A.
+// If the first character in string s is in the set {b,c,d,f,g}, then return B.
+// If the first character in string s is in the set {h,j,k,l,m}, then return C.
+// If the first character in string s is in the set {n,p,q,r,s,t,v,w,x,y,z}, then return D.
 
-// Locked stub code in the editor reads a single integer, , from stdin and passes it to a function named factorial.
-
-const factorial = (n) => {
-  // the factorial of 0 and 1 is 1
-  if (n === 0 || n === 1) {
-    return 1;
+const getLetter = (s) => {
+  console.log(s);
+  switch (s.charAt(0)) {
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+      console.log("A");
+      break;
+    case "b":
+    case "c":
+    case "d":
+    case "f":
+    case "g":
+      console.log("B");
+      break;
+    case "h":
+    case "j":
+    case "k":
+    case "l":
+    case "m":
+      console.log("C");
+      break;
+    case "n":
+    case "p":
+    case "q":
+    case "r":
+    case "s":
+    case "t":
+    case "w":
+    case "x":
+    case "y":
+    case "z":
+      console.log("D");
+      break;
+    default:
+      console.log("Please try again");
   }
-  // loop through each value, starting at one less than n and ending at 1
-  for (let i = n - 1; i >= 1; i--) {
-    n *= i;
-  }
-  return n;
 };
