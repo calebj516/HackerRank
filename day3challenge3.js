feca63fbd71a33baffbf6f96b91eb6332c3e4609
Caleb Jones <calebj516@gmail.com>
2021-04-16 21:04:55 -0500

    Day 3: Throw

---
+++ day3challenge3.js
@@ -0,0 +1,24 @@
+// Day 3: Throw
+
+// Task
+
+// Complete the isPositive function below. It has one integer parameter, a. If the value of a is positive, it must return the string YES. Otherwise, it must throw an Error according to the following rules:
+
+// If a is 0, throw an Error with message = Zero Error.
+// If a is negative, throw an Error with message = Negative Error.
+
+const isPositive = (a) => {
+  if (a > 0) {
+    return "YES";
+  } else if (a === 0) {
+    throw new Error("Zero Error");
+  } else {
+    throw new Error("Negative Error");
+  }
+};
+
+// Tests below
+
+// console.log(isPositive(10));
+// console.log(isPositive(0));
+// console.log(isPositive(-10));
