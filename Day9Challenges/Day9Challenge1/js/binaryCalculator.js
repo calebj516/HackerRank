467454801466e203d93b14be34e5521502267163
Caleb Jones <calebj516@gmail.com>
2021-05-11 20:16:20 -0500

    Day 9 Binary Calculator: completed JS functionality

--- Day9Challenges/Day9Challenge1/js/binaryCalculator.js
+++ Day9Challenges/Day9Challenge1/js/binaryCalculator.js
@@ -1,5 +1,5 @@
 let resArr = [];
-let leftOperand, rightOperand;
+let leftOperand, rightOperand, operator, expression;
 let res = document.getElementById("res");
 let btn0 = document.getElementById("btn0");
 let btn1 = document.getElementById("btn1");
@@ -22,13 +22,44 @@ const clearRes = () => {
 };
 
 const sumRes = () => {
-  if (resArr) {
-    let leftOperand = resArr.join("");
-    clearRes();
-    console.log(leftOperand);
+  if (res.innerHTML) {
+    resArr.push("+");
+    res.innerHTML = resArr.join("");
+    operator = "+";
   }
-  //   if (leftOperand && !resArr) {
-  //     let rightOperand = resArr.join("");
-  //     console.log(rightOperand);
-  //   }
+};
+
+const subRes = () => {
+  if (res.innerHTML) {
+    resArr.push("-");
+    res.innerHTML = resArr.join("");
+    operator = "-";
+  }
+};
+
+const mulRes = () => {
+  if (res.innerHTML) {
+    resArr.push("*");
+    res.innerHTML = resArr.join("");
+    operator = "*";
+  }
+};
+const divRes = () => {
+  if (res.innerHTML) {
+    resArr.push("/");
+    res.innerHTML = resArr.join("");
+    operator = "/";
+  }
+};
+
+const evalRes = () => {
+  if (res.innerHTML) {
+    let leftOperand = resArr.slice(0, resArr.indexOf(operator)).join(""); // takes result array from start up to the operator
+    let rightOperand = resArr
+      .slice(resArr.indexOf(operator) + 1, resArr.length)
+      .join(""); // takes result array from the position after the operator to the end of the array
+    let base10Res = eval(parseInt(leftOperand, 2) + parseInt(rightOperand, 2));
+    let binaryRes = base10Res.toString(2);
+    res.innerHTML = binaryRes;
+  }
 };
